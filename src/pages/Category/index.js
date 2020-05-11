5770184c1dc1fee3bb72b4a22aef64f0160cea91
mengxuan <3007492783@qq.com>
2020-05-11 14:34:51 +0800

    修改分类页面逻辑

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -10,56 +10,54 @@ import './index.scss'
 
 const colors = ['magenta', 'geekblue', 'purple', 'cyan', 'volcano', 'blue', 'green', 'orange']
 const Category = props => {
-	const { category, fetchCategory } = props
-	const { Container, LeftWrapper, RightWrapper } = useTwoColLayout(),
-		list = category.toJS()
-	useEffect(() => {
-		if (!list.length) {
-			fetchCategory()
-		}
-	}, [])
-	return (
-		<div className="category-wrapper">
-			<Container>
-				<LeftWrapper>
-					<div className="category-title">
-						<span className='name'>分类</span>
-						<p className='disc'>目前共计{list.length}个分类</p>
-					</div>
-					{
-						!!list.length ?
-							list.map(item => {
-								const index = Math.floor(Math.random() * 8),
-									color = colors[index],
-									{ name, articleCount } = item
-								return (
-									<Tag
-										color={color}
-										size="large"
-										key={item._id}>
-										<Link
-											to={`/categories/${name}`}>
-											{name}({articleCount})
-										</Link>
-									</Tag>
-								)
-							}) :
-							<Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>
-					}
-				</LeftWrapper>
-				<RightWrapper></RightWrapper>
-			</Container>
-		</div>
-	)
+    const { category, fetchCategory } = props
+    const { Container, LeftWrapper, RightWrapper } = useTwoColLayout(),
+        list = category.toJS()
+    useEffect(() => {
+        fetchCategory()
+    }, [])
+    return (
+        <div className="category-wrapper">
+            <Container>
+                <LeftWrapper>
+                    <div className="category-title">
+                        <span className='name'>分类</span>
+                        <p className='disc'>目前共计{list.length}个分类</p>
+                    </div>
+                    {
+                        !!list.length ?
+                            list.map(item => {
+                                const index = Math.floor(Math.random() * 8),
+                                    color = colors[index],
+                                    { name, articleCount } = item
+                                return (
+                                    <Tag
+                                        color={color}
+                                        size="large"
+                                        key={item._id}>
+                                        <Link
+                                            to={`/categories/${name}`}>
+                                            {name}({articleCount})
+                                        </Link>
+                                    </Tag>
+                                )
+                            }) :
+                            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>
+                    }
+                </LeftWrapper>
+                <RightWrapper></RightWrapper>
+            </Container>
+        </div>
+    )
 }
 
 
 export default compose(
-	memo,
-	connect(
-		state => ({
-			category: state.getIn(['category', 'categoryList'])
-		}),
-		actions
-	)
+    memo,
+    connect(
+        state => ({
+            category: state.getIn(['category', 'categoryList'])
+        }),
+        actions
+    )
 )(Category)
--- src/pages/CategoryDetail/index.js
+++ src/pages/CategoryDetail/index.js
@@ -2,7 +2,7 @@ import React, { memo, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
-import { Timeline } from 'antd';
+import { Timeline, Empty } from 'antd';
 import useTwoColLayout from '@hooks/useTwoColLayout'
 import { actions } from "@store/reducers/categoryReducer"
 import { dateFormat } from '@api/utils'
@@ -12,59 +12,64 @@ import './index.scss'
 const { Item } = Timeline
 
 const CategoryDetail = props => {
-	const { Container, LeftWrapper, RightWrapper } = useTwoColLayout()
-	const { category, fetchCategory } = props,
-		list = category.toJS(),
-		params = useParams()
-	useEffect(() => {
-		// if (!list.length) {
-			fetchCategory(params)
-		// }
-	}, [])
-	console.log(list);
-	return (
-		<div className="category-detail_wrapper">
-			<Container>
-				<LeftWrapper>
-					<div className="category-detail-content">
-						<div className="title">
-							<span className="name">{params.name}</span>
-							分类
-						</div>
-						<div className="info">
-							<Timeline>
-								{
-									list.map(item => (
-										<Item
-											key={item._id}>
+    const { Container, LeftWrapper, RightWrapper } = useTwoColLayout()
+    const { category, fetchCategory } = props,
+        list = category.toJS(),
+        params = useParams()
+    useEffect(() => {
+        fetchCategory(params)
+    }, [])
+    return (
+        <div className="category-detail_wrapper">
+            <Container>
+                <LeftWrapper>
+                    <div className="category-detail-content">
+                        <div className="title">
+                            <span className="name">{params.name}</span>
+                            分类
+                        </div>
+                        {
+                            !!list.length ? (
+                                <div className="info">
+                                    <Timeline>
+                                        {
+                                            list.map(item => (
+                                                <Item
+                                                    key={item._id}>
 											<span className="time">
 												{dateFormat(item.createdAt)}
 											</span>
-											<span className="title">
+                                                    <span className="title">
 												<Link to={`/articleDetail/${item._id}`}>
 													{item.title}
 												</Link>
 											</span>
-										</Item>
-									))
-								}
-							</Timeline>
-						</div>
-					</div>
-				</LeftWrapper>
-				<RightWrapper></RightWrapper>
-			</Container>
-		</div>
-	)
+                                                </Item>
+                                            ))
+                                        }
+                                    </Timeline>
+                                </div>
+                            ) : (
+                                <Empty
+                                    description="该分类下还没有文章哦"
+                                    image={Empty.PRESENTED_IMAGE_SIMPLE}/>
+                            )
+                        }
+                    </div>
+                </LeftWrapper>
+                <RightWrapper></RightWrapper>
+            </Container>
+        </div>
+    )
 }
 
 export default compose(
-	memo,
-	connect(
-		state => ({
-			category: state.getIn(['category', 'categoryList'])
-		}),
-		actions
-	)
+    memo,
+    connect(
+        state => ({
+            category: state.getIn(['category', 'categoryList'])
+        }),
+        actions
+    )
 )(CategoryDetail)
 
